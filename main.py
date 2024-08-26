1f383bca99a222d6b3ec9d3211f27fd69373817c
2cryp <smirnov187007811@gmail.com>
2024-08-26 06:58:40 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+homework_count=12
+print('всего задач:',homework_count)
+hourse_spent_count=1.5
+print('затрачено часов:',hourse_spent_count)
+course_name=':Python'
+print('курс:',course_name)
+time_for_one_task=(1.5/12)
+print('среднее время выполения',time_for_one_task,'часа')
